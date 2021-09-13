ab49fe38ab565334b38e6af5076b9043e5a0dd78
Deepak Sain <deepak.sain@lmdconsulting.com>
2021-09-13 18:12:25 +0530

    Correction on mongodb configuration

--- src/config/production.config.ts
+++ src/config/production.config.ts
@@ -12,7 +12,8 @@ export default class ProductionConfig implements IConfig {
         preflightContinue: false,
         optionsSuccessStatus: 204,
     }
-    public mongoConnectionURL = 'mongodb+srv://deepaksain:sain.deepak21@cluster0.hmzaa.mongodb.net/management?retryWrites=true&w=majority' // 'mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>'
+    // public mongoConnectionURL = 'mongodb+srv://deepaksain:sain.deepak21@cluster0.hmzaa.mongodb.net/management?retryWrites=true&w=majority' // 'mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>'
+    public mongoConnectionURL = 'mongodb://localhost:27017/management' // 'mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>'
     public BCRYPT_SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS || 12)
     public JWT_SECRET: string = process.env.JWT_SECRET || 'asdjl;jkljraas}asdkhjas{hahkliweui'
     public JWT_EXPIRESIN: string = process.env.JWT_EXPIRESIN || '24h'
